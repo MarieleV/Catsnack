93f3d4d4b3c7263d4178cb39fb0747e0af752275
Dereckkk1 <128764884+Dereckkk1@users.noreply.github.com>
2023-06-15 19:37:25 -0300

    Delete Catsnack directory

--- Catsnack/Logincadastro/connect_catsnack.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('America/Sao_Paulo');
-
-try {
-    $pdo = new PDO("mysql:dbname=catsnack;host=localhost;charset=utf8", "root", "");
-} catch (PDOException $erro) {
-    echo ("ERRO NA CONEXÃO: <br>" . $erro->getMessage());
-}
-?>
